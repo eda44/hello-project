9b4a5f60e583892a8b895977f96377643971358b
Daniil Epikhov <depikhov29@gmail.com>
2023-07-31 18:29:04 +0300

    initial commit

---
+++ src/main/java/ru.eda/configuration/AppConfig.java
@@ -0,0 +1,12 @@
+package ru.eda.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.eda")
+public class AppConfig implements WebMvcConfigurer {
+}
---
+++ src/main/java/ru.eda/configuration/DispatcherInitialization.java
@@ -0,0 +1,20 @@
+package ru.eda.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherInitialization extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
